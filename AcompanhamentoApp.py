fc14cdb0feaa725296b0db6517d65ff31f7ab315
wmoural <68879544+wmoural@users.noreply.github.com>
2023-05-04 15:16:30 -0300

    Update AcompanhamentoApp.py

--- AcompanhamentoApp.py
+++ AcompanhamentoApp.py
@@ -2,6 +2,14 @@ import leafmap.foliumap as leafmap
 import pandas as pd
 import streamlit as st
 
-m = leafmap.Map()
 
-m.to_streamlit(width=800)
+with st.sidebar:
+    st.title('Acompanhamento de pesquisas')
+    st.selectbox('1','2','3')
+ 
+
+with st.columns(1):
+        
+    m = leafmap.Map()
+    
+    m.to_streamlit(width=800)
